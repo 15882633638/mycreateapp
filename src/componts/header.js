f2ca2c778fd073aee9d3c0c0f0ea0271d6b69d5e
unknown <741097812@qq.com>
2019-11-23 23:32:44 +0800

    js跳转路由

--- src/componts/header.js
+++ src/componts/header.js
@@ -1,17 +1,24 @@
 import React from 'react';
 
+import { withRouter } from 'react-router';
 
 class Header extends React.Component{
     constructor(props){
         super(props);
+        console.log(props)
         this.state = {
             name:'这是header组件'
         }
         
     }
+    href(){
+        console.log(this.props)
+        const {history} = this.props;
+        history.push('/home')
+    }
     render(){
         return (
-            <div>
+            <div onClick = {this.href.bind(this)}>
                 <h1>{this.state.name}</h1>
                 {this.props.content}
             </div>
